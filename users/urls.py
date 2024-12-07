da0c912648bc135d5d3fb4fbaafba647e479bde2
Khalil <khalilundo12@gmail.com>
2024-12-07 09:22:59 +0800

    job fair server error

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # user autherntication
     # sign in
     path('', views.home, name="home"),
-    path('jobfair', views.job_fair, name="jobfair"),
+    path('jobfair/', views.job_fair, name="jobfair"),
     
     path('login/', views.login_user, name="login"),
     path('logout/', views.logout_user, name="logout"),
